b6c72f53e9c10e99fa20d41eb217af792b9b057b
Guicai-Li <guicai.li.china@gmail.com>
2015-08-06 14:13:11 +0800

    add UITextField

---
+++ Categories/UITextField+Shake.h
@@ -0,0 +1,61 @@
+//
+//  UITextField+Shake.h
+//  UITextField+Shake
+//
+//  Created by Andrea Mazzini on 08/02/14.
+//  Copyright (c) 2014 Fancy Pixel. All rights reserved.
+//
+
+typedef NS_ENUM(NSInteger, ShakeDirection) {
+    ShakeDirectionHorizontal = 0,
+    ShakeDirectionVertical
+};
+
+@interface UITextField (Shake)
+
+/**-----------------------------------------------------------------------------
+ * @name UITextField+Shake
+ * -----------------------------------------------------------------------------
+ */
+
+/** Shake the UITextField
+*
+* Shake the text field a given number of times
+*
+* @param times The number of shakes
+* @param delta The width of the shake
+*/
+- (void)shake:(int)times withDelta:(CGFloat)delta;
+
+/** Shake the UITextField at a custom speed
+ *
+ * Shake the text field a given number of times with a given speed
+ *
+ * @param times The number of shakes
+ * @param delta The width of the shake
+ * @param interval The duration of one shake
+ */
+- (void)shake:(int)times withDelta:(CGFloat)delta andSpeed:(NSTimeInterval)interval;
+
+/** Shake the UITextField at a custom speed
+ *
+ * Shake the text field a given number of times with a given speed
+ *
+ * @param times The number of shakes
+ * @param delta The width of the shake
+ * @param interval The duration of one shake
+ * @param direction of the shake
+ */
+- (void)shake:(int)times withDelta:(CGFloat)delta andSpeed:(NSTimeInterval)interval shakeDirection:(ShakeDirection)shakeDirection;
+
+@end
+
+@interface UIButton (Shake)
+
+- (void)shake:(int)times withDelta:(CGFloat)delta;
+
+- (void)shake:(int)times withDelta:(CGFloat)delta andSpeed:(NSTimeInterval)interval;
+
+- (void)shake:(int)times withDelta:(CGFloat)delta andSpeed:(NSTimeInterval)interval shakeDirection:(ShakeDirection)shakeDirection;
+
+@end
---
+++ Categories/UITextField+Shake.m
@@ -0,0 +1,83 @@
+//
+//  UITextField+Shake.m
+//  UITextField+Shake
+//
+//  Created by Andrea Mazzini on 08/02/14.
+//  Copyright (c) 2014 Fancy Pixel. All rights reserved.
+//
+
+#import "UITextField+Shake.h"
+
+@implementation UITextField (Shake)
+
+- (void)shake:(int)times withDelta:(CGFloat)delta
+{
+	[self _shake:times direction:1 currentTimes:0 withDelta:delta andSpeed:0.08 shakeDirection:ShakeDirectionHorizontal];
+}
+
+- (void)shake:(int)times withDelta:(CGFloat)delta andSpeed:(NSTimeInterval)interval
+{
+	[self _shake:times direction:1 currentTimes:0 withDelta:delta andSpeed:interval shakeDirection:ShakeDirectionHorizontal];
+}
+
+- (void)shake:(int)times withDelta:(CGFloat)delta andSpeed:(NSTimeInterval)interval shakeDirection:(ShakeDirection)shakeDirection
+{
+    [self _shake:times direction:1 currentTimes:0 withDelta:delta andSpeed:interval shakeDirection:shakeDirection];
+}
+
+- (void)_shake:(int)times direction:(int)direction currentTimes:(int)current withDelta:(CGFloat)delta andSpeed:(NSTimeInterval)interval shakeDirection:(ShakeDirection)shakeDirection
+{
+	[UIView animateWithDuration:interval animations:^{
+		self.transform = (shakeDirection == ShakeDirectionHorizontal) ? CGAffineTransformMakeTranslation(delta * direction, 0) : CGAffineTransformMakeTranslation(0, delta * direction);
+	} completion:^(BOOL finished) {
+		if(current >= times) {
+			self.transform = CGAffineTransformIdentity;
+			return;
+		}
+		[self _shake:(times - 1)
+		   direction:direction * -1
+		currentTimes:current + 1
+		   withDelta:delta
+			andSpeed:interval
+         shakeDirection:shakeDirection];
+	}];
+}
+
+@end
+
+@implementation UIButton (Shake)
+
+- (void)shake:(int)times withDelta:(CGFloat)delta
+{
+    [self _shake:times direction:1 currentTimes:0 withDelta:delta andSpeed:0.03 shakeDirection:ShakeDirectionHorizontal];
+}
+
+- (void)shake:(int)times withDelta:(CGFloat)delta andSpeed:(NSTimeInterval)interval
+{
+    [self _shake:times direction:1 currentTimes:0 withDelta:delta andSpeed:interval shakeDirection:ShakeDirectionHorizontal];
+}
+
+- (void)shake:(int)times withDelta:(CGFloat)delta andSpeed:(NSTimeInterval)interval shakeDirection:(ShakeDirection)shakeDirection
+{
+    [self _shake:times direction:1 currentTimes:0 withDelta:delta andSpeed:interval shakeDirection:shakeDirection];
+}
+
+- (void)_shake:(int)times direction:(int)direction currentTimes:(int)current withDelta:(CGFloat)delta andSpeed:(NSTimeInterval)interval shakeDirection:(ShakeDirection)shakeDirection
+{
+    [UIView animateWithDuration:interval animations:^{
+        self.transform = (shakeDirection == ShakeDirectionHorizontal) ? CGAffineTransformMakeTranslation(delta * direction, 0) : CGAffineTransformMakeTranslation(0, delta * direction);
+    } completion:^(BOOL finished) {
+        if(current >= times) {
+            self.transform = CGAffineTransformIdentity;
+            return;
+        }
+        [self _shake:(times - 1)
+           direction:direction * -1
+        currentTimes:current + 1
+           withDelta:delta
+            andSpeed:interval
+      shakeDirection:shakeDirection];
+    }];
+}
+
+@end
